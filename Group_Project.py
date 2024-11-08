c3efafcbee4d16703772f7ce86348b9e634f5124
Pau En Piang <mrpiangpi007@gmail.com>
2024-11-08 14:13:21 -0800

    Menu

---
+++ Group_Project.py
@@ -0,0 +1,15 @@
+'''
+CS 131: Python Programming 1
+Project: Student Record Management System
+Team Member 1 name
+Team Member 2 name
+'''
+print("Student Record Management System")
+print("--------------------------------")
+print("1. Add a new student record")
+print("2. Delete a student record")
+print("3. Search for a student record")
+print("4. Display all student records")
+print("5. Exit")
+
+
